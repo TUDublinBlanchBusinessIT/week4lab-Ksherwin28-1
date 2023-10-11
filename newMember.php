eed9ef34e52902197c70dd1478cb3d58de2dcb8b
shero121 <keithsherwin6764@gmail.com>
2023-10-11 10:03:58 +0100

    added newMember.html

---
+++ newMember.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form method="GET" action="processForm.php">
+            <p>Enter your name</p>
+            <input name="firstName"/>
+            <br>
+            <input name="surName"/>
+            <br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
